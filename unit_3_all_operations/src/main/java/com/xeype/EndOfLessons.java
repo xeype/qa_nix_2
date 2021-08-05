81582d876f37d5823523f31e35605f4898670381
Alexey Pedko <alexeypedko@gmail.com>
2021-08-05 20:14:12 +0300

    Uncommented While

--- unit_3_all_operations/src/main/java/com/xeype/EndOfLessons.java
+++ unit_3_all_operations/src/main/java/com/xeype/EndOfLessons.java
@@ -7,11 +7,11 @@ public class EndOfLessons {
         Scanner scanner = new Scanner(System.in);
         byte countOfLessons = 0;
 
-        System.out.println("Enter the number of lessons(1-10): ");
-
-//         Checking the number of lessons (Loops banned by task condition)
-//        while(countOfLessons > 10 || countOfLessons <= 0)
+//         Checking the number of lessons
+        while(countOfLessons > 10 || countOfLessons <= 0) {
+            System.out.println("Enter the number of lessons(1-10): ");
             countOfLessons = scanner.nextByte();
+        }
 
         int totalMinutes = countOfLessons * 45 + (countOfLessons / 2) * 5 + ((countOfLessons + 1) / 2 - 1) * 15;
         System.out.println("Lessons end at: " + (totalMinutes / 60 + 9) + " " + (totalMinutes % 60));
